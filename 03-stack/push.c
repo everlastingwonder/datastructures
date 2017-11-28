affe2e37be7608c77504694dd4d02e27030bd562
everlastingwonder <guswiedey@gmail.com>
2017-11-27 23:00:48 -0500

    blah

--- 03-stack/push.c
+++ 03-stack/push.c
@@ -1,3 +1,11 @@
+/****
+* Gus Wiedey
+* Assignment 03
+* Data Structures
+* 11/27/2017
+* push.c
+****/
+
 #include <mystack.h>
 
 void push(Stack *s, int val) {
@@ -5,6 +13,6 @@ void push(Stack *s, int val) {
     s->data[s->top] = val;
     s->top++;
   } else {
-    printf("ERROR: Cannot push to stack because stack is full!");
+    printf("ERROR: Cannot push to stack because stack is full!\n");
   }
 }
--- 03-stack/top.c
+++ 03-stack/top.c
@@ -1,3 +1,11 @@
+/****
+* Gus Wiedey
+* Assignment 03
+* Data Structures
+* 11/27/2017
+* top.c
+****/
+
 #include <mystack.h>
 
 int top(Stack *s) {
@@ -6,6 +14,7 @@ int top(Stack *s) {
     int val = stack->data[i];
     return val;
   } else {
+    printf("ERROR: Cannot fetch top value because stack is empty!\n");
     return HUGE_VAL;
   }
 }
